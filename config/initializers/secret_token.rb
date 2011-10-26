2645edfcab4baadf11f48c28050b70ab933f4eb2
Dan Kempton <dankempton@gmail.com>
2011-10-26 13:29:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimelineBuild::Application.config.secret_token = 'cccaf7f00bbf3782a547f48ba660e0e0a70b1faf16f8a8508ba37474ba0522845f09be15dde408f894be9c549af117249a45104113194e9a974254b34e7a0f0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TimelineBuild::Application.config.session_store :cookie_store, :key => '_timeline-build_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TimelineBuild::Application.config.session_store :active_record_store
